e919dc73e7d1dd20345f1ccdcf06d8e74bf91c43
sachin.dhiman <40537108+sachinndhimann@users.noreply.github.com>
2024-08-13 22:08:32 +0530

    Update entrypoint.sh

--- edge-flask-simulator/entrypoint.sh
+++ edge-flask-simulator/entrypoint.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-# Load KVM module if not already loaded
-if command -v lsmod &> /dev/null && command -v modprobe &> /dev/null; then
-    if ! lsmod | grep -q kvm; then
-        modprobe kvm
-    fi
-    if ! lsmod | grep -q kvm_intel; then
-        modprobe kvm_intel
-    fi
-else
-    echo "kmod tools (lsmod/modprobe) not available, skipping KVM module loading"
-fi
+# Ensure no other process is using the QEMU image
+fuser -k /var/lib/libvirt/images/edge_device.img
 
 # Start the VM in headless mode (no graphics)
-qemu-system-x86_64 -m 1024 -hda /var/lib/libvirt/images/edge_device.img -cdrom /tmp/alpine.iso -boot d -enable-kvm -nographic &
+#qemu-system-x86_64 -no-kvm -m 1024 -hda /var/lib/libvirt/images/edge_device.img -cdrom /tmp/alpine.iso -boot d -nographic &
+qemu-system-x86_64 -no-kvm -m 2048  -hda /var/lib/libvirt/images/edge_device.img \
+-netdev user,id=n0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=n0 \
+-cdrom /tmp/alpine.iso -boot d -nographic &
+# Wait for the VM to boot and SSH service to start
+echo "Waiting for vm to boot........"
+sleep 50
 
-sleep 60
+# Start SSH service
+service ssh start
+echo "ssh started..."
+# Wait for SSH service to initialize
+sleep 20
 
-# Start the VM in headless mode for further commands
-qemu-system-x86_64 -m 1024 -hda /var/lib/libvirt/images/edge_device.img -enable-kvm -nographic &
+ssh -o ServerAliveInterval=60 root@localhost -p 2222
+# Execute SSH commands
+sshpass -p 'your_password' ssh -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 root@localhost -p 2222 "apk add docker && rc-update add docker boot && service docker start"
 
-ssh root@localhost -p 2222 "apk add docker && rc-update add docker boot && service docker start"
-
+# Transfer and run Docker container
 scp -P 2222 /tmp/flask_api_app.tar root@localhost:/tmp/
-ssh root@localhost -p 2222 "docker load -i /tmp/flask_api_app.tar && docker run -d -p 5000:5000 --name flask_api_app_container flask_api_app"
+sshpass -p 'your_password' ssh -o StrictHostKeyChecking=no root@localhost -p 2222 "docker load -i /tmp/flask_api_app.tar && docker run -d -p 5000:5000 --name flask_api_app_container flask_api_app"
 
+# Keep the container running
 tail -f /dev/null
